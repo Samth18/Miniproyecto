6c3ebd210c63f184a71db4d30244dd99bdbc6899
Samu ESRI <escobar.samuel@correounivalle.edu.co>
2023-10-02 20:17:52 -0500

    Elimine archivo duplicado

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       System.out.println("-----Datos Personales-----");
+       System.out.println();
+
+       
+       
+
+    }   
+
+}
---
+++ src/Ciudades.java
@@ -0,0 +1,4 @@
+public enum Ciudades {
+    Cali, Palmira, Yumbo, Tuluá, Buenaventura, Cartago,
+    Buga, Jamundi, Andalucia, Bugalagrande
+}
