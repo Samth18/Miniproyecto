c4444a294f5214cd05dae3796c62bfc64217b3f1
Samu ESRI <escobar.samuel@correounivalle.edu.co>
2023-10-02 20:17:52 -0500

    Elimine archivo duplicado

---
+++ src/Partidos.java
@@ -0,0 +1,4 @@
+public enum Partidos {
+    Centro_Democratico, Pacto_Historico, Partido_Liberal, Partido_Conservador, Cambio_Radical, Partido_De_la_U,
+    Alianza_Verde, Polo_Democratico, Union_Patriotica, Fuerza_De_La_Paz
+}
