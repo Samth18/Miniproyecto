b9789696553433f256affa034222a4b1a1b14993
Joseph <144748605+ImMavz@users.noreply.github.com>
2023-10-08 19:29:35 -0500

    Se agregó el método de actualizar candidato.
    Se agrega el método de actualizar candidato en base a su cedula ingresada permitiendo así al usuario actualizar todas las credenciales del candidato seleccionado.

--- miniproyecto_1/src/Crud.java
+++ miniproyecto_1/src/Crud.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 public class Crud {
@@ -158,12 +159,227 @@ public class Crud {
 
 
 
+//-----------------------------------------------------------
 
 
 
 
 
+    public static void ActualizarCandidato(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite la cédula del candidato que desea modificar: ");
+        String cedulaModificar = scanner.nextLine();
+
+        Candidato candidatoModificar = buscarCandidatoPorCedula(listaCandidato, cedulaModificar);
+
+        if (candidatoModificar != null) {
+            System.out.println("Candidato encontrado:");
+            System.out.println("Nombre actual: " + candidatoModificar.getNombre());
+            System.out.println("Cedula actual: " + candidatoModificar.getCedula());
+            System.out.println("Ciudad de procedencia actual: " + candidatoModificar.getCiudades());
+            System.out.println("Ideologia actual: " + candidatoModificar.getIdeologia());
+            System.out.println("Partido politico actual: " + candidatoModificar.getPartido());
+            System.out.println("Promesas actuales: " + candidatoModificar.getPromesas());
+            System.out.println("Numero de votos actuales: " + candidatoModificar.getVotos());
+            System.out.println("");
 
 
+            System.out.print("Nuevo nombre: ");
+            String nuevoNombre = scanner.nextLine();
+            candidatoModificar.setNombre(nuevoNombre);
+
+            System.out.print("Nueva cedula: ");
+            String nuevaCedula = scanner.nextLine();
+            candidatoModificar.setCedula(nuevaCedula);
+
+            System.out.println("Nueva ciudad de procedencia: ");
+            System.out.println();
+            System.out.println("1. Cali           2. Palmira         3. Yumbo\n" +
+                    "4. Tuluá          5. Buenaventura    6. Cartago\n" +
+                    "7. Jamundi        8. Buga            9. Andalucia\n" +
+                    "10. Bugalagrande");
+            System.out.println("0. Salir");
+            System.out.print("Digite una opcion: ");
+            byte opcionCiudad = scanner.nextByte();
+            scanner.nextLine();
+            System.out.println();
+            Ciudades ciudadElegida = null;
+    
+            switch (opcionCiudad) {
+                case 1:
+                    ciudadElegida = Ciudades.Cali;
+                    break;
+                case 2:
+                    ciudadElegida = Ciudades.Palmira;
+                    break;
+                case 3:
+                    ciudadElegida = Ciudades.Yumbo;
+                    break;
+                case 4: 
+                    ciudadElegida = Ciudades.Tuluá;
+                    break;
+                case 5: 
+                    ciudadElegida = Ciudades.Buenaventura;
+                    break;
+                case 6: 
+                    ciudadElegida = Ciudades.Cartago;
+                    break;
+                case 7: 
+                    ciudadElegida = Ciudades.Jamundi;
+                    break;
+                case 8: 
+                    ciudadElegida = Ciudades.Buga;
+                    break;
+                case 9: 
+                    ciudadElegida = Ciudades.Andalucia;
+                    break;
+                case 10: 
+                    ciudadElegida = Ciudades.Bugalagrande;
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opcion incorrecta");
+            }
+            if (ciudadElegida != null) {
+                if (candidatoModificar != null) {
+                    candidatoModificar.setCiudades(ciudadElegida);
+                }
+            System.out.println("---Ideologia---");
+            System.out.println();
+            System.out.println("1. Derecha");
+            System.out.println("2. Izquierda");
+            System.out.print("Digite una Opcion: ");
+            byte opcionIdeologia = scanner.nextByte();
+            scanner.nextLine();
+            Ideologia ideologiaElegida = null;
+        
+            switch (opcionIdeologia) {
+                case 1:
+                    ideologiaElegida = Ideologia.Derecha;
+                    break;
+                case 2:
+                    ideologiaElegida = Ideologia.Izquierda;
+                    break;
+                default:
+                    System.out.println("Opcion incorrecta");
+                }
+        
+            Partidos partidoElegido = null;
+        
+            if (ideologiaElegida != null) {
+                System.out.println();
+                System.out.println("---Partidos politicos---");
+                System.out.println();
+                switch (ideologiaElegida) {
+                    case Derecha:
+                        System.out.println("1. Centro Democratico");
+                        System.out.println("2. Partido Conservador");
+                        System.out.println("3. Cambio Radical");
+                        System.out.println("4. Partido de la U");
+                        break;
+                    case Izquierda:
+                        System.out.println("1. Pacto Historico");
+                        System.out.println("2. Partido Liberal");
+                        System.out.println("3. Alianza Verde");
+                        System.out.println("4. Polo Democratico");
+                        System.out.println("5. Union Patriotica");
+                        System.out.println("6. Fuerza de la Paz");
+                        break;
+                    }
+        
+                System.out.println("0. Salir");
+                System.out.print("Digite una opcion: ");
+                byte opcionPartido = scanner.nextByte();
+                scanner.nextLine();
+                System.out.println();
+        
+                switch (ideologiaElegida) {
+                    case Derecha:
+                        switch (opcionPartido) {
+                            case 1:
+                                partidoElegido = Partidos.Centro_Democratico;
+                                break;
+                            case 2:
+                                partidoElegido = Partidos.Partido_Conservador;
+                                break;
+                            case 3:
+                                partidoElegido = Partidos.Cambio_Radical;
+                                break;
+                            case 4:
+                                partidoElegido = Partidos.Partido_De_la_U;
+                                break;
+                            case 0:
+                                break;
+                            default:
+                                    System.out.println("Opcion incorrecta");
+                            }
+                            break;
+                    case Izquierda:
+                        switch (opcionPartido) {
+                            case 1:
+                                partidoElegido = Partidos.Pacto_Historico;
+                                break;
+                            case 2:
+                                partidoElegido = Partidos.Partido_Liberal;
+                                break;
+                            case 3:
+                                partidoElegido = Partidos.Alianza_Verde;
+                                break;
+                            case 4:
+                                partidoElegido = Partidos.Polo_Democratico;
+                                break;
+                            case 5:
+                                partidoElegido = Partidos.Union_Patriotica;
+                                break;
+                            case 6:
+                                partidoElegido = Partidos.Fuerza_De_La_Paz;
+                                break;
+                            case 0:
+                                break;
+                            default:
+                                System.out.println("Opcion incorrecta");
+                            }
+                        break; 
+                    }
+                }
+        
+                // Ahora tienes la ideología y el partido seleccionados
+                System.out.println("Ideologia actualizada ");
+                System.out.println("Partido actualizado ");
+            }
+        }
+
+        
+            System.out.print("Nuevas promesas: ");
+            String nuevasPromesas = scanner.nextLine();
+            candidatoModificar.setPromesas(nuevasPromesas);
+
+            System.out.print("Nuevos votos: ");
+            int votosActualizados = scanner.nextInt();
+            candidatoModificar.setVotos(votosActualizados);
+            
+
+            System.out.print("");
+
+            System.out.println("Candidato actualizado:");
+            System.out.println("Nuevo nombre: " + candidatoModificar.getNombre());
+            System.out.println("Nueva cedula: "+ candidatoModificar.getCedula());
+            System.out.println("Nuvea ciudad: "+ candidatoModificar.getCiudades());
+            System.out.println("Nueva ideologia: " + candidatoModificar.getIdeologia());
+            System.out.println("Nuevo partido politico: " + candidatoModificar.getPartido());
+            System.out.println("Nuevas promesas: " + candidatoModificar.getPromesas());
+            System.out.println("Votos actualizados: " + candidatoModificar.getVotos());
+        }
+    
+    public static Candidato buscarCandidatoPorCedula(List<Candidato> listaCandidato, String cedula) {
+        for (Candidato candidato : listaCandidato) {
+            if (candidato.getCedula().equals(cedula)) {
+                return candidato;
+            }
+        }
+        return null;
+    }
+
+    
 }
-
